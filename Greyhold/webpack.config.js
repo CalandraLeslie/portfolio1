310f46fabb32be3ca372dbb2b49d573d68f1e5fb
Calandra Leslie <121008739+CalandraLeslie@users.noreply.github.com>
2025-04-10 15:22:43 +0200

    update to fix 404

--- Greyhold/webpack.config.js
+++ Greyhold/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/Greyhold/' // MUST include trailing slash
+    publicPath: '/' // Change from '/Greyhold/' to '/'
   },
   module: {
     rules: [

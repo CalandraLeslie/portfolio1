f6f73344eb062f30a939f66b97ccc6ed6d69c0bf
Calandra Leslie <121008739+CalandraLeslie@users.noreply.github.com>
2025-04-10 15:34:27 +0200

    test

--- Greyhold/webpack.config.js
+++ Greyhold/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/Greyhold/' // Change from '/' to '/Greyhold/'
+    publicPath: '/' // Change from '/Greyhold/' to '/'
   },
   module: {
     rules: [

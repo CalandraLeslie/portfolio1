c5e3148b2bde83de481692bdc85abb955f0cf4af
Calandra Leslie <121008739+CalandraLeslie@users.noreply.github.com>
2025-04-10 15:06:42 +0200

    maybe this will work

--- Greyhold/src/app.js
+++ Greyhold/src/app.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router basename="/">
+    <Router basename="/Greyhold">
       <div className="app">
         <Navbar />
         <Switch>
--- Greyhold/webpack.config.js
+++ Greyhold/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/' // Change from '/Greyhold/' to '/'
+    publicPath: '/Greyhold/' // MUST include trailing slash
   },
   module: {
     rules: [
